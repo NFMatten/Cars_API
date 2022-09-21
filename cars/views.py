5eaed04bbea3c8cedc1ed2a34e0ba831c121e992
Nikolaus Matten <nfmatten@gmail.com>
2022-09-21 15:08:37 -0600

    Created method to Post + corrected error in serializer + created admin

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
-    
-    
 
-    return Response('ok')
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serialzer = CarSerializer(cars, many=True)
+        return Response(serialzer.data)
+
+    elif request.method == 'POST':
+        serialzer = CarSerializer(data=request.data)
+        if serialzer.is_valid() == True:
+            serialzer.save()
+            return Response(serialzer.data, status=201)
+        else:
+            return Response(serialzer.errors, status=400)
