fe46da124410d8ccedad65a9e31842a3a56e9c72
Nikolaus Matten <nfmatten@gmail.com>
2022-09-21 15:44:45 -0600

    Completed functionailty for Car Views File.

--- cars/views.py
+++ cars/views.py
@@ -22,11 +22,11 @@ def cars_list(request):
         return Response(serialzer.data, status=status.HTTP_201_CREATED)
        
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def car_detail(request, pk):
 
     car = get_object_or_404(Car, pk=pk)
-    
+
     if request.method == 'GET':
         serializer = CarSerializer(car)
         return Response(serializer.data)
@@ -36,3 +36,7 @@ def car_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
